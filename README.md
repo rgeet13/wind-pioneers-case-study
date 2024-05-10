a18ac256fbf7e6f96cef502e85dcbfc700509ffb
geet.ramchandani <geet.ramchandani@webshar.org>
2024-05-10 14:29:11 +0530

    Setup django project with ASGI server and installed daphne and channels

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # wind-pioneers-case-study
+
---
+++ file_based_QnA_with_ChatGPT/file_based_QnA_with_ChatGPT/asgi.py
@@ -0,0 +1,24 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'file_based_QnA_with_ChatGPT.settings')
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from django.urls import path
+
+django_asgi_app = get_asgi_application()
+
+
+
+
+websocket_urlpatterns = [
+]
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
